406f1338ee28f1d724f624c9109160fadd412d76
vita2333 <lp_vitadolce@163.com>
2020-05-13 15:02:38 +0800

    counter , counter with 2 parts

--- lib/main.dart
+++ lib/main.dart
@@ -139,13 +139,96 @@ class MyScaffold extends StatelessWidget {
             MyAppBar(
                 title: Text('example title',
                     style: Theme.of(context).primaryTextTheme.headline6)),
-            Expanded(
-                child: Center(
+            Center(
               child: Text('Hello world'),
-            ))
+            ),
+            Center(
+              child: Counter(),
+            ),
+            Center(
+              child: Counter2Part(),
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('add'),
+        ),
+        Text('Count $_counter')
+      ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(onPressed: onPressed, child: Text('add'));
+  }
+}
+
+class Counter2Part extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState2Part();
+  }
+}
+
+class _CounterState2Part extends State<Counter2Part> {
+  int _counter = 0;
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CounterIncrementor(
+          onPressed: _increment,
+        ),
+        CounterDisplay(
+          count: _counter,
+        )
+      ],
     );
   }
 }
