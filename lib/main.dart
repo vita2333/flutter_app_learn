aba47ec52ae645a9d9cd856454bd821d1c6f24c7
vita2333 <lp_vitadolce@163.com>
2020-05-13 11:07:01 +0800

    add heart

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = new Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
-    final wp = WordPair.random();
     return Scaffold(
         appBar: AppBar(
           title: Text('startup name generator'),
@@ -38,10 +39,16 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
