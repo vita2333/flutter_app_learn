cf7a9d81fdf37932200fb17b653d875b6eeed133
vita2333 <lp_vitadolce@163.com>
2020-05-13 14:33:18 +0800

    material widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: 'My app',
+      home: MyScaffold(),
+    ));
 
-class MyApp extends StatelessWidget {
+class ListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: RandomWords(),
+      theme: new ThemeData(primaryColor: Colors.white),
     );
   }
 }
@@ -90,5 +94,55 @@ class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() {
     return RandomWordsState();
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: null,
+            tooltip: 'Navigation menu',
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+              title: Text('example title',
+                  style: Theme.of(context).primaryTextTheme.headline6)),
+          Expanded(
+              child: Center(
+            child: Text('Hello world'),
+          ))
+        ],
+      ),
+    );
   }
 }
